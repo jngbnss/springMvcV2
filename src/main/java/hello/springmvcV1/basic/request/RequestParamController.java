73020aa5ff5d3ea4547f83c19cdc2ea0d0b0133f
jngbn <[user email]>
2025-09-19 12:11:46 +0900

    save

---
+++ src/main/java/hello/springmvcV1/basic/request/RequestParamController.java
@@ -0,0 +1,19 @@
+package hello.springmvcV1.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Controller
+public class RequestParamController {
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response)throws IOException{
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        response.getWriter().write("ok");
+    }
+}
